5fb013a279a9c3e312e1c0832a6d8fdde8ed3320
TankSao <38127423+TankSao@users.noreply.github.com>
2018-10-27 11:07:29 +0800

    2018-10-27

---
+++ ImageShakeDemo/app/src/main/java/com/example/administrator/imageshakedemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.administrator.imageshakedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private  ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView)findViewById(R.id.img);
+        img.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        int num = 0;
+        shake.setRepeatCount(3);
+        view.startAnimation(shake);
+    }
+}
